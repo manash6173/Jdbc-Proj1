45b950f33107cb62ee2b9e6356c204cd0959d564
dev1 <dev1@gmail.com>
2019-03-31 22:57:25 +0530

    Dav2-Created both DAO and service class

--- GitHubJdbcProjTest/src/main/java/com/manash/dao/EmployeeDAO.java
+++ GitHubJdbcProjTest/src/main/java/com/manash/dao/EmployeeDAO.java
@@ -1,5 +1,26 @@
 package com.manash.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.manash.utility.ConnUtility;
+
 public class EmployeeDAO {
+	
+	private ConnUtility conn=new ConnUtility();
+	
+	public int getMaxSal() throws SQLException {
+		PreparedStatement ps=null;
+		int result=0;
+		Connection con=conn.getConnecton("jdbc:oracle:thin:@localhost:1521:xe", "system", "manash");
+		ps=con.prepareStatement("select max(sal) from emp");
+		ResultSet rs=ps.executeQuery();
+		while(rs.next()) {
+			 result=rs.getInt(1);
+		}
+		return result;
+	}
 
 }
--- GitHubJdbcProjTest/src/main/java/com/manash/service/EmployeeService.java
+++ GitHubJdbcProjTest/src/main/java/com/manash/service/EmployeeService.java
@@ -1,5 +1,17 @@
 package com.manash.service;
 
+import java.sql.SQLException;
+
+import com.manash.dao.EmployeeDAO;
+
 public class EmployeeService {
+	private EmployeeDAO dao=new EmployeeDAO();
+	
+	public int getEmpHighSal() throws SQLException{
+		//use dao
+		int sal=dao.getMaxSal();
+		return sal;
+	}
+	
 
 }
